3e13ec86bf59422693f7e93c5b7318639133d3f7
Daniel Marin <danielarturomt@gmail.com>
2021-09-20 14:08:18 +0100

    feat(angular): set up a basic angular sample application

---
+++ apps/demobase-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demobase-root',
+  template: `
+    <header>
+      <h1>Hello world!</h1>
+    </header>
+  `,
+  styles: [],
+})
+export class AppComponent {}
