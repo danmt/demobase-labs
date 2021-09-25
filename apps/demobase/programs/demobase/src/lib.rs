61dba7c965dcf13ad4d4d636dca3a43a2d37c3cf
Daniel Marin <danielarturomt@gmail.com>
2021-09-25 18:03:24 +0100

    refactor(demobase): massive clean up for the program

--- apps/demobase/programs/demobase/src/lib.rs
+++ apps/demobase/programs/demobase/src/lib.rs
@@ -15,40 +15,45 @@ pub mod demobase {
 
     pub fn create_collection(ctx: Context<CreateCollection>, name: String, bump: u8) -> ProgramResult {
         msg!("Create collection");
-        ctx.accounts.collection.bump = bump;
         ctx.accounts.collection.name = parse_string(name);
+        ctx.accounts.collection.bump = bump;
         ctx.accounts.collection.authority = ctx.accounts.authority.key();
         ctx.accounts.collection.application = ctx.accounts.application.key();
         Ok(())
     }
 
-    pub fn create_collection_attribute(ctx: Context<CreateCollectionAttribute>, name: String, attribute_type: String, size: u8, bump: u8) -> ProgramResult {
+    pub fn create_collection_attribute(ctx: Context<CreateCollectionAttribute>, name: String, kind: u8, modifier: u8, size: u8, bump: u8) -> ProgramResult {
         msg!("Create collection attribute");
-        ctx.accounts.collection_attribute.name = parse_string(name);
-        ctx.accounts.collection_attribute.bump = bump;
-        ctx.accounts.collection_attribute.attribute_type = parse_string(attribute_type);
-        ctx.accounts.collection_attribute.size = size;
-        ctx.accounts.collection_attribute.authority = ctx.accounts.authority.key();
-        ctx.accounts.collection_attribute.collection = ctx.accounts.collection.key();
+        ctx.accounts.attribute.name = parse_string(name);
+        ctx.accounts.attribute.bump = bump;
+        ctx.accounts.attribute.kind = AttributeKind::from(kind)?;
+        ctx.accounts.attribute.modifier = AttributeKindModifier::from(modifier, size)?;
+        ctx.accounts.attribute.authority = ctx.accounts.authority.key();
+        ctx.accounts.attribute.collection = ctx.accounts.collection.key();
+        ctx.accounts.attribute.application = ctx.accounts.application.key();
         Ok(())
     }
 
     pub fn create_collection_instruction(ctx: Context<CreateCollectionInstruction>, name: String, bump: u8) -> ProgramResult {
         msg!("Create collection instruction");
-        ctx.accounts.collection_instruction.name = parse_string(name);
-        ctx.accounts.collection_instruction.bump = bump;
-        ctx.accounts.collection_instruction.authority = ctx.accounts.authority.key();
-        ctx.accounts.collection_instruction.collection = ctx.accounts.collection.key();
+        ctx.accounts.instruction.name = parse_string(name);
+        ctx.accounts.instruction.bump = bump;
+        ctx.accounts.instruction.authority = ctx.accounts.authority.key();
+        ctx.accounts.instruction.collection = ctx.accounts.collection.key();
+        ctx.accounts.instruction.application = ctx.accounts.application.key();
         Ok(())
     }
 
-    pub fn create_collection_instruction_argument(ctx: Context<CreateCollectionInstructionArgument>, name: String, argument_type: String, bump: u8) -> ProgramResult {
+    pub fn create_collection_instruction_argument(ctx: Context<CreateInstructionArgument>, name: String, kind: u8, modifier: u8, size: u8, bump: u8) -> ProgramResult {
         msg!("Create collection instruction argument");
-        ctx.accounts.collection_instruction_argument.name = parse_string(name);
-        ctx.accounts.collection_instruction_argument.argument_type = parse_string(argument_type);
-        ctx.accounts.collection_instruction_argument.bump = bump;
-        ctx.accounts.collection_instruction_argument.authority = ctx.accounts.authority.key();
-        ctx.accounts.collection_instruction_argument.collection_instruction = ctx.accounts.collection_instruction.key();
+        ctx.accounts.argument.name = parse_string(name);
+        ctx.accounts.argument.kind = AttributeKind::from(kind)?;
+        ctx.accounts.argument.modifier = AttributeKindModifier::from(modifier, size)?;
+        ctx.accounts.argument.bump = bump;
+        ctx.accounts.argument.authority = ctx.accounts.authority.key();
+        ctx.accounts.argument.instruction = ctx.accounts.instruction.key();
+        ctx.accounts.argument.collection = ctx.accounts.collection.key();
+        ctx.accounts.argument.application = ctx.accounts.application.key();
         Ok(())
     }
 
@@ -60,16 +65,19 @@ pub mod demobase {
         ctx.accounts.account.authority = ctx.accounts.authority.key();
         ctx.accounts.account.instruction = ctx.accounts.instruction.key();
         ctx.accounts.account.collection = ctx.accounts.collection.key();
+        ctx.accounts.account.application = ctx.accounts.application.key();
         Ok(())
     }
 
     pub fn create_account_bool_attribute(ctx: Context<CreateAccountBoolAttribute>, kind: u8, bump: u8) -> ProgramResult {
         msg!("Create account bool attribute");
-        ctx.accounts.attribute.bump = bump;
         ctx.accounts.attribute.kind = AccountBoolAttributeKind::from(kind)?;
+        ctx.accounts.attribute.bump = bump;
+        ctx.accounts.attribute.authority = ctx.accounts.authority.key();
         ctx.accounts.attribute.account = ctx.accounts.account.key();
         ctx.accounts.attribute.instruction = ctx.accounts.instruction.key();
-        ctx.accounts.attribute.authority = ctx.accounts.authority.key();
+        ctx.accounts.attribute.collection = ctx.accounts.collection.key();
+        ctx.accounts.attribute.application = ctx.accounts.application.key();
         Ok(())
     }
 
@@ -105,11 +113,11 @@ pub struct CreateCollection<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32,
+        space = 8 + 32 + 32 + 32 + 1,
         seeds = [
             b"collection",
-            name.as_bytes(),
-            application.key().as_ref()
+            application.key().as_ref(),
+            name.as_bytes()
         ],
         bump = bump
     )]
@@ -121,20 +129,22 @@ pub struct CreateCollection<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(name: String, attribute_type: String, size: u8, bump: u8)]
+#[instruction(name: String, kind: u8, modifier: u8, size: u8, bump: u8)]
 pub struct CreateCollectionAttribute<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32 + 32 + 1,
+        space = 8 + 32 + 32 + 32 + 32 + 2 + 2 + 1,
         seeds = [
             b"collection_attribute",
+            application.key().as_ref(),
             collection.key().as_ref(),
             name.as_bytes()
         ],
         bump = bump
     )]
-    pub collection_attribute: Box<Account<'info, CollectionAttribute>>,
+    pub attribute: Box<Account<'info, CollectionAttribute>>,
+    pub application: Box<Account<'info, Application>>,
     pub collection: Box<Account<'info, Collection>>,
     #[account(mut)]
     pub authority: Signer<'info>,
@@ -147,15 +157,17 @@ pub struct CreateCollectionInstruction<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32,
+        space = 8 + 32 + 32 + 32 + 32 + 1,
         seeds = [
             b"collection_instruction",
+            application.key().as_ref(),
             collection.key().as_ref(),
             name.as_bytes()
         ],
         bump = bump
     )]
-    pub collection_instruction: Box<Account<'info, CollectionInstruction>>,
+    pub instruction: Box<Account<'info, CollectionInstruction>>,
+    pub application: Box<Account<'info, Application>>,
     pub collection: Box<Account<'info, Collection>>,
     #[account(mut)]
     pub authority: Signer<'info>,
@@ -163,21 +175,25 @@ pub struct CreateCollectionInstruction<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(name: String, argument_type: String, bump: u8)]
-pub struct CreateCollectionInstructionArgument<'info> {
+#[instruction(name: String, kind: u8, modifier: u8, array_size: u8, bump: u8)]
+pub struct CreateInstructionArgument<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32 + 32,
+        space = 8 + 32 + 32 + 32 + 32 + 32 + 2 + 2 + 1,
         seeds = [
-            b"collection_instruction_argument",
-            collection_instruction.key().as_ref(),
+            b"instruction_argument",
+            application.key().as_ref(),
+            collection.key().as_ref(),
+            instruction.key().as_ref(),
             name.as_bytes()
         ],
         bump = bump
     )]
-    pub collection_instruction_argument: Box<Account<'info, CollectionInstructionArgument>>,
-    pub collection_instruction: Box<Account<'info, CollectionInstruction>>,
+    pub argument: Box<Account<'info, InstructionArgument>>,
+    pub application: Box<Account<'info, Application>>,
+    pub collection: Box<Account<'info, Collection>>,
+    pub instruction: Box<Account<'info, CollectionInstruction>>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -189,17 +205,20 @@ pub struct CreateInstructionAccount<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32 + 32 + 1,
+        space = 8 + 32 + 32 + 32 + 32 + 32 + 2 + 1,
         seeds = [
             b"instruction_account",
+            application.key().as_ref(),
+            collection.key().as_ref(),
             instruction.key().as_ref(),
             name.as_bytes()
         ],
         bump = bump
     )]
     pub account: Box<Account<'info, InstructionAccount>>,
-    pub instruction: Box<Account<'info, CollectionInstruction>>,
+    pub application: Box<Account<'info, Application>>,
     pub collection: Box<Account<'info, Collection>>,
+    pub instruction: Box<Account<'info, CollectionInstruction>>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -211,16 +230,21 @@ pub struct CreateAccountBoolAttribute<'info> {
     #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 1 + 32 + 32 + 1,
+        space = 8 + 32 + 32 + 32 + 32 + 32 + 2 + 1,
         seeds = [
             b"account_bool_attribute",
+            application.key().as_ref(),
+            collection.key().as_ref(),
+            instruction.key().as_ref(),
             account.key().as_ref()
         ],
         bump = bump
     )]
     pub attribute: Box<Account<'info, AccountBoolAttribute>>,
-    pub account: Box<Account<'info, InstructionAccount>>,
+    pub application: Box<Account<'info, Application>>,
+    pub collection: Box<Account<'info, Collection>>,
     pub instruction: Box<Account<'info, CollectionInstruction>>,
+    pub account: Box<Account<'info, InstructionAccount>>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -251,55 +275,63 @@ pub struct Application {
 #[account]
 pub struct Collection {
     pub authority: Pubkey,
-    pub bump: u8,
     pub application: Pubkey,
     pub name: [u8; 32],
+    pub bump: u8,
 }
 
 #[account]
 pub struct CollectionAttribute {
     pub authority: Pubkey,
-    pub bump: u8,
+    pub application: Pubkey,
     pub collection: Pubkey,
     pub name: [u8; 32],
-    pub attribute_type: [u8; 32],
-    pub size: u8,
+    pub kind: AttributeKind,
+    pub modifier: AttributeKindModifier,
+    pub bump: u8,
 }
 
 #[account]
 pub struct CollectionInstruction {
     pub authority: Pubkey,
-    pub bump: u8,
+    pub application: Pubkey,
     pub collection: Pubkey,
     pub name: [u8; 32],
+    pub bump: u8,
 }
 
 #[account]
-pub struct CollectionInstructionArgument {
+pub struct InstructionArgument {
     pub authority: Pubkey,
-    pub bump: u8,
-    pub collection_instruction: Pubkey,
+    pub application: Pubkey,
+    pub collection: Pubkey,
+    pub instruction: Pubkey,
     pub name: [u8; 32],
-    pub argument_type: [u8; 32],
+    pub kind: AttributeKind,
+    pub modifier: AttributeKindModifier,
+    pub bump: u8,
 }
 
 #[account]
 pub struct InstructionAccount {
     pub authority: Pubkey,
-    pub bump: u8,
+    pub application: Pubkey,
     pub collection: Pubkey,
     pub instruction: Pubkey,
     pub name: [u8; 32],
     pub kind: AccountKind,
+    pub bump: u8,
 }
 
 #[account]
 pub struct AccountBoolAttribute {
     pub authority: Pubkey,
-    pub bump: u8,
+    pub application: Pubkey,
+    pub collection: Pubkey,
     pub instruction: Pubkey,
     pub account: Pubkey,
     pub kind: AccountBoolAttributeKind,
+    pub bump: u8,
 }
 
 pub fn parse_string(string: String) -> [u8; 32] {
@@ -307,6 +339,63 @@ pub fn parse_string(string: String) -> [u8; 32] {
     let mut data = [0u8; 32];
     data[..src.len()].copy_from_slice(src);
     return data;
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, PartialEq)]
+pub enum AttributeKind {
+    U8 {
+      size: u8
+    },
+    U16 {
+      size: u8
+    },
+    U32 {
+      size: u8
+    },
+    U64 {
+      size: u8
+    },
+    U128 {
+      size: u8
+    },
+    Pubkey {
+      size: u8
+    },
+}
+
+impl AttributeKind {
+    fn from(index: u8) -> Result<Self> {
+        match index {
+            0 => Ok(AttributeKind::U8 { size: 1 }),
+            1 => Ok(AttributeKind::U16 { size: 2 }),
+            2 => Ok(AttributeKind::U32 { size: 4 }),
+            3 => Ok(AttributeKind::U64 { size: 8 }),
+            4 => Ok(AttributeKind::U128 { size: 16 }),
+            5 => Ok(AttributeKind::Pubkey { size: 32 }),
+            _ => Err(ErrorCode::InvalidAttributeKind.into()),
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, PartialEq)]
+pub enum AttributeKindModifier {
+    None,
+    Array {
+      size: u8
+    },
+    Vector
+}
+
+impl AttributeKindModifier {
+    fn from(index: u8, size: u8) -> Result<Self> {
+        match index {
+            0 => Ok(AttributeKindModifier::None),
+            1 => Ok(AttributeKindModifier::Array { size: size }),
+            2 => Ok(AttributeKindModifier::Vector),
+            _ => Err(ErrorCode::InvalidAttributeKindModifier.into()),
+        }
+    }
 }
 
 #[repr(u8)]
@@ -349,6 +438,10 @@ impl AccountBoolAttributeKind {
 
 #[error]
 pub enum ErrorCode {
+    #[msg("Invalid attribute kind")]
+    InvalidAttributeKind,
+    #[msg("Invalid attribute kind modifier")]
+    InvalidAttributeKindModifier,
     #[msg("Invalid account kind")]
     InvalidAccountKind,
     #[msg("Invalid account attribute kind")]
